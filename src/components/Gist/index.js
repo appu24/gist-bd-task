2d77f0621ff0e94050c4d40758c7f51ae42b78a8
Aparajitha Kalyan <aparajitha.kalyan@gmail.com>
2020-12-30 01:39:59 +0400

    Code optimisation

--- src/components/Gist/index.js
+++ src/components/Gist/index.js
@@ -7,8 +7,6 @@ import {
 } from 'react-router-dom';
 import {
   Spin,
-  Collapse,
-  Typography,
   Input,
   Table,
   Space,
@@ -30,7 +28,9 @@ const { Column } = Table;
 const { Search } = Input;
 const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
-export default () => {
+const Gist = () => {
+
+  // Declaring state variables for Gist details, pagination and search attributes
   const [gist, setGist] = useState([]);
   const [page, setPage] = useState(1);
   const [pageTotal, setPageTotal] = useState(100);
@@ -41,53 +41,21 @@ export default () => {
   useEffect(() => {
     setLoading(true);
     getGist(search);
-  }, [page]);
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page, searchBy]);
+
+  // Function to get list of Gists
   const getGist = async (searchKey) => {
     try {
       setLoading(true);
+      setSearch(searchKey);
+
       if(searchBy === "username" && searchKey && searchKey !== '') {
         let g = await octokit.gists.listForUser({
           username: searchKey, //"nayelyzarazua-bluetrail"
           per_page: 10,
           page: page
-        });
-        setPageTotal(100);
-        setGist(g.data);
-      } else {
-        setSearchBy('gistID');
-        let g = await octokit.gists.listPublic({
-          per_page: 10,
-          page: page
-        });
-        setPageTotal(1000);
-        setGist(g.data);
-      }
-    } catch(err) {
-      console.log("error - ", err);
-      setGist([]);
-      setPageTotal(0);
-    }
-    setLoading(false);
-  };
-
-  const onSearch = async (searchKey) => {
-    try {
-      setLoading(true);
-      setSearch(searchKey);
-
-      if(!searchKey || searchKey === "") {
-        getGist(searchKey);
-        return;
-      }
-
-      setPage(1);
-
-      if(searchBy === "username") {
-        let g = await octokit.gists.listForUser({
-          username: searchKey, //"nayelyzarazua-bluetrail"
-          per_page: 10,
-          page: 1
         });
         setGist(g.data);
         if(g.data.length < 10) {
@@ -96,11 +64,21 @@ export default () => {
           setPageTotal(100);
         }
       } else {
-        let g = await octokit.gists.get({
-          gist_id: searchKey, // 44ce722a0dd04e000ca79955a2dbca71
-        });
-        setGist([g.data]);
-        setPageTotal(1);
+        setSearchBy('gistID');
+        if(searchKey && searchKey !== '') {
+          let g = await octokit.gists.get({
+            gist_id: searchKey, // 44ce722a0dd04e000ca79955a2dbca71
+          });
+          setGist([g.data]);
+          setPageTotal(1);
+        } else {
+          let g = await octokit.gists.listPublic({
+            per_page: 10,
+            page: page
+          });
+          setPageTotal(1000);
+          setGist(g.data);
+        }
       }
     } catch(err) {
       console.log("error - ", err);
@@ -110,8 +88,15 @@ export default () => {
     setLoading(false);
   };
 
+  // Function initiated on Page change
   const onPageChange = (page, pageSize) => {
     setPage(page);
+  };
+
+  // Function initiated on change on Search-by option
+  const onChangeOfSearchBy = (data) => {
+    setPage(1);
+    setSearchBy(data.target.value)
   };
 
   return <Layout page={"gist"}>
@@ -120,12 +105,12 @@ export default () => {
         <Search
           placeholder={`Search by ${(searchBy === "username" ? "Username" : "Gist ID")}`}
           allowClear
-          onSearch={onSearch}
+          onSearch={getGist}
           enterButton
         />
-        <Radio.Group value={searchBy} onChange={(data) => {setSearchBy(data.target.value)}}>
-          <Radio.Button value="gistID">Gist ID</Radio.Button>
-          <Radio.Button value="username">Username</Radio.Button>
+        <Radio.Group value={searchBy} onChange={onChangeOfSearchBy}>
+          <Radio.Button value="gistID" key="gistID">Gist ID</Radio.Button>
+          <Radio.Button value="username" key="username">Username</Radio.Button>
         </Radio.Group>
       </SearchBlock>
       {(loading) ? <Spin indicator={antIcon} /> : <>
@@ -162,3 +147,5 @@ export default () => {
     </GistBlock>
   </Layout>
 };
+
+export default Gist;
